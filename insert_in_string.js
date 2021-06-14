bd44cd96899146738cf1865fa70ca8be78d911f1
david <calhoun5321@gmail.com>
2021-06-14 13:23:17 -0700

    adds interpolation and concatination practice

---
+++ insert_in_string.js
@@ -0,0 +1,37 @@
+// # 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+// var firstName = "colin";
+// var lastName = "frederick";
+// console.log("my baby " + firstName + " " + lastName + " is sleepy");
+
+// # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+// console.log(`my baby ${firstName} ${lastName} is very sleepy `);
+
+// # 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
+// # 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+// var color1 = "pink";
+// var color2 = "orange";
+// var color3 = "blue";
+// console.log(color1 + " and " + color2 + " and " + color3);
+
+// # 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+// console.log(`my favorite colors are ${color1} ${color2} ${color3}`);
+
+// # 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+
+// # 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+var bookTitle = "Autobiography of A Yogi";
+var author = "Yogananda";
+console.log(bookTitle + " is by " + author);
+
+// # 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
+
+console.log(`${bookTitle} is by ${author}`);
+
+// # 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
+
+// # 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
+var city1 = "SF";
+var city2 = "Boston";
+var city3 = "Healdsburg";
+console.log("I then asked, do you want to travel to " + city1 + ", " + city2 + " or " + city3 + "?");
