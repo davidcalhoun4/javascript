b63fc13d61b9ffc384549324577c484df4e6cc06
david <calhoun5321@gmail.com>
2021-06-19 14:42:10 -0700

    changes names of files to js convention, adds methods file

---
+++ stringAndNumberMethods.js
@@ -0,0 +1,51 @@
+// # 1. use a variable to store a word, then prints that word with all capital letters.
+// var word = "Hello There";
+// console.log(word.toUpperCase());
+
+// # 2. use a variable to store a number, then prints "That's a big number" if the number is greater than 100.
+// var number = 44;
+// if (number > 100) {
+//   console.log("That is a big number!");
+// }
+
+// # 3. use variables to store two numbers, then prints the numbers added together.
+// var num1 = 33;
+// var num2 = 44;
+// console.log(num1 + num2);
+
+// # 4. use a variable to store a word, then prints that word in reverse order.
+// function reverseString(str) {
+//   var reversed = str.split("").reverse().join("");
+//   console.log(reversed);
+// }
+// reverseString("Betty");
+
+// # 5. use a variable to store a number, then prints the number times 10.
+// var times = 10;
+// for (var num = 1; num <= times; num++) {
+//   console.log(num);
+// }
+
+// # 6. use variables to store two words, then prints both words on the same line in all capital letters.
+// function upCase(word1, word2) {
+//   console.log(word1.toUpperCase(), word2.toUpperCase());
+// }
+// upCase("good", "bye");
+
+// # 7. use a variable to store a word, then prints the number of letters in the word.
+// function letterCount(string) {
+//   if (string.length === 1) {
+//     console.log("Your word contains 1 letter");
+//   } else if (string.length > 2) {
+//     console.log(`Your word contains ${string.length} letters`);
+//   }
+// }
+// letterCount("beast");
+
+// # 8. use a variable to store a number, then prints "That's a negative number" if the number is less than 0.
+
+// # 9. use variables to store two numbers, then prints the two numbers multiplied together.
+
+// # 10. use a variable to store a word, then prints "That's a long word" if the word has more than 5 letters.
+
+// # SOLUTIONS: https://gist.github.com/peterxjang/1539a3ad79728ba4fb68dd8d07279c29
